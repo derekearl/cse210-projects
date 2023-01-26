c5e5bc683862787a63c78d7998246d9962811f44
derekearl <ear17004@byui.edu>
2023-01-26 14:59:11 -0700

    Week1update

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,3 +1,5 @@
+//Derek Earl assignment 1//
+//January 2023 BYU-Idaho//
 using System;
 
 class Program
@@ -9,5 +11,37 @@ class Program
         Console.Write("What is your last name? ");
         string lname = Console.ReadLine();
         Console.WriteLine($"Your name is: {lname}, {fname} {lname}. ");
+
+        Console.Write("What is your grade? ");
+        string grade = Console.ReadLine();
+        int percent = int.Parse(grade);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
     }
 }
