c8fa59d36cda94a7b7bfafa5133d2217c9b63365
derekearl <ear17004@byui.edu>
2023-02-02 22:45:52 -0700

    updatedproveassignment

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,50 @@
-using System;
+//List for the journal and selection screen.
 
-class Program
+using System;
+using System.IO;
+
+public class Display
 {
-    static void Main(string[] args)
+     public static void Main(string[] args)
+    {   
+        Write write = new Write();
+        deFileLoad deLoadEntry = new deFileLoad();
+        string entry = "";
+        string deUserEntry = "";
+        Console.WriteLine ("Welcome to the journal!");
+        Console.WriteLine ("Please select one of the following.");
+    while(deUserEntry!="5"){
+    var deDisplayList = new List<string>(4);
     {
-        Console.WriteLine("Hello Develop02 World!");
+        deDisplayList.Add ("1. Write");
+        deDisplayList.Add ("2. Display");
+        deDisplayList.Add ("3. Load");
+        deDisplayList.Add ("4. Save");
+        deDisplayList.Add ("5. Quit");
+
+        for(int i=0;i<deDisplayList.Count;i++)
+        {
+            Console.WriteLine(deDisplayList[i]);
+        }
+    }
+        deUserEntry = Console.ReadLine();
+        if(deUserEntry == "1"){
+            //get prompt
+            //entry += write.getInput();
+            //Console.WriteLine(entry);
+            Prompt prompt = new Prompt();
+            prompt.getEntry();
+        }
+        else if(deUserEntry == "2"){
+            Console.WriteLine(entry);
+        }
+        else if(deUserEntry == "3"){
+            entry += deLoadEntry.load();
+        }
+
+    }
     }
 }
+    
+
+
---
+++ prove/Develop02/write.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Write{
+    public string getInput(){
+    Console.Write ("> ");
+    string entry = Console.ReadLine();
+    return entry;
+    }
+
+}
+
